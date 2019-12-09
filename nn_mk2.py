510afcd9209dc4dd764d5b321866e4a570d8cb53
SwagGothamCoder <skiingrunner5879@gmail.com>
2019-12-08 19:11:44 -0700

    Resolved error where weights were not being updated in each iteration. New class works great for models with 1 hidden layer and the categorical cross entropy loss function.

--- nn_mk2.py
+++ nn_mk2.py
@@ -38,23 +38,31 @@ class Neural_Network(object):
     #w2 = (y_dim, h_dim)
     #b1 = (h_dim, 1)
     #b2 = (y_dim, 1)
-    def backprop(self, x, y, learning_rate):
-        w1,w2,b1,b2 = self.params
+    def backprop(self, x, y, learning_rate, batch_size, num_epochs):
         #m = float(y.shape[1])
-        m = float(x.shape[1])
-        a1, z2, a2 = self.forward_prop(x)
-        dw2 = np.dot((a2-y), a1.T)
-        db2 = np.sum((a2-y), axis=1, keepdims=True)
-        # round 2
-        dCda1 = np.dot(w2.T, (a2 - y))
-        da1dz1 = (1.0 - np.power(a1, 2))
-        dz1dw1 = x.T
-        dw1 = np.dot(np.multiply(dCda1, da1dz1), dz1dw1)
-        db1 = np.sum(np.multiply(dCda1, da1dz1), axis=1, keepdims=True)
-        w1 = w1 - ((learning_rate / m) * dw1)
-        w2 = w2 - ((learning_rate / m) * dw2)
-        b1 = b1 - ((learning_rate / m) * db1)
-        b2 = b2 - ((learning_rate / m) * db2)
+        training_size = float(x.shape[1])
+        m = batch_size
+        num_batches = int(training_size / batch_size)
+        x = np.array(np.split(x.T, num_batches))
+        y = np.array(np.split(y.T, num_batches))
+        for e in range(num_epochs):
+            for i in range(num_batches):
+                w1,w2,b1,b2 = self.params
+                x_batch = x[i].T
+                y_batch = y[i].T
+                a1, z2, a2 = self.forward_prop(x_batch)
+                dw2 = np.dot((a2-y_batch), a1.T)
+                db2 = np.sum((a2-y_batch), axis=1, keepdims=True)
+                # round 2
+                dCda1 = np.dot(w2.T, (a2 - y_batch))
+                da1dz1 = (1.0 - np.power(a1, 2))
+                dz1dw1 = x_batch.T
+                dw1 = np.dot(np.multiply(dCda1, da1dz1), dz1dw1)
+                db1 = np.sum(np.multiply(dCda1, da1dz1), axis=1, keepdims=True)
+                w1 = w1 - ((learning_rate / m) * dw1)
+                w2 = w2 - ((learning_rate / m) * dw2)
+                b1 = b1 - ((learning_rate / m) * db1)
+                b2 = b2 - ((learning_rate / m) * db2)
+                self.params = [w1, w2, b1, b2]
         #print('Cost: ', cost(x, y))
         #print('Accuracy: ', accuracy(x, y))
-        return [w1, w2, b1, b2]
